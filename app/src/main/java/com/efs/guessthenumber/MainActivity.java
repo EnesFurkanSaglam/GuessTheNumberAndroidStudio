f198adc3e12bd4c2cef01e31816d878fdcd51db3
Enes Furkan SAGLAM <enesfurkansaglam@gmail.com>
2024-02-07 21:19:46 +0300

    The first part of the project is finished

--- app/src/main/java/com/efs/guessthenumber/MainActivity.java
+++ app/src/main/java/com/efs/guessthenumber/MainActivity.java
@@ -3,12 +3,23 @@ package com.efs.guessthenumber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewRemainingRight;
+    private EditText editTextNumber;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnGuess(View view){
+
     }
 }
